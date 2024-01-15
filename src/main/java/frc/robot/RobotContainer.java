148292d10474fd69197b29004524dbc923280c71
Ben Alderson <ben.c.alderson@proton.me>
2024-01-15 17:56:52 -0500

    Redo swerve code for blackout

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,29 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.OIConstants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.TeleopDrive;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
+
+  private final Joystick m_driverController = new Joystick(OperatorConstants.kDriverControllerPort);
+  private final Joystick m_operatorController = new Joystick(OperatorConstants.kOperatorControllerPort);
+  
+  private DriveSubsystem driveSubsystem = new DriveSubsystem();
+
   public RobotContainer() {
+    driveSubsystem.setDefaultCommand(new TeleopDrive(driveSubsystem,
+        () -> MathUtil.applyDeadband(m_driverController.getRawAxis(XboxController.Axis.kLeftX.value), OIConstants.kDriveDeadband),
+        () -> MathUtil.applyDeadband(m_driverController.getRawAxis(XboxController.Axis.kLeftY.value), OIConstants.kDriveDeadband),
+        () -> MathUtil.applyDeadband(m_driverController.getRawAxis(XboxController.Axis.kRightX.value), OIConstants.kDriveDeadband)
+    ));
     configureBindings();
   }
 
---
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
+
+/**
+ * This command takes operator control from the driver and controls the swerve
+ * motors with that control signal.
+ */
+public class TeleopDrive extends Command {
+  private DriveSubsystem driveSubsystem;
+  private DoubleSupplier xSpeedSupplier;
+  private DoubleSupplier ySpeedSupplier;
+  private DoubleSupplier rotSpeedSupplier;
+
+  /** Creates a new TeleopDrive. */
+  public TeleopDrive(DriveSubsystem driveSubsystem,
+      DoubleSupplier xSpeedSupplier,
+      DoubleSupplier ySpeedSupplier,
+      DoubleSupplier rotSpeedSupplier) {
+    this.driveSubsystem = driveSubsystem;
+    addRequirements(driveSubsystem);
+    this.xSpeedSupplier = xSpeedSupplier;
+    this.ySpeedSupplier = ySpeedSupplier;
+    this.rotSpeedSupplier = rotSpeedSupplier;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveSubsystem.drive(xSpeedSupplier.getAsDouble(), ySpeedSupplier.getAsDouble(), rotSpeedSupplier.getAsDouble(),
+        true, true);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveSubsystem.drive(0, 0, 0, false, false);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
