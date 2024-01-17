706bd90af2eb482d8d5d803a37205db9827e4aeb
4039-zach <118681764+4039-zach@users.noreply.github.com>
2024-01-17 16:44:00 -0500

    Add PathPlannerLib dependancy

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,19 +7,38 @@ package frc.robot;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import frc.robot.Constants.OIConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.TeleopDrive;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
+  // Create the "Main" tab first so it will be first in the list.
+  public final ShuffleboardTab mainTab = Shuffleboard.getTab("Main");
+
+  // The robot's subsystems and commands are defined here...
+  private DriveSubsystem driveSubsystem = new DriveSubsystem();
+  
+  // Create the "About" tab last so it will be last in the list.
+  public final ShuffleboardTab aboutTab = Shuffleboard.getTab("About");
 
   private final Joystick m_driverController = new Joystick(OperatorConstants.kDriverControllerPort);
   private final Joystick m_operatorController = new Joystick(OperatorConstants.kOperatorControllerPort);
-  
-  private DriveSubsystem driveSubsystem = new DriveSubsystem();
+
+  private final int translationAxis = XboxController.Axis.kLeftY.value;
+  private final int strafeAxis = XboxController.Axis.kLeftX.value;
+  private final int rotationXAxis = XboxController.Axis.kRightX.value;
+  private final int rotationYAxis = XboxController.Axis.kRightY.value;
+
+
+  private final SendableChooser<Command> autoChooser;
 
   public RobotContainer() {
     driveSubsystem.setDefaultCommand(new TeleopDrive(driveSubsystem,
@@ -28,11 +47,17 @@ public class RobotContainer {
         () -> MathUtil.applyDeadband(m_driverController.getRawAxis(XboxController.Axis.kRightX.value), OIConstants.kDriveDeadband)
     ));
     configureBindings();
+
+    autoChooser = AutoBuilder.buildAutoChooser();
+
+    mainTab.add("Auto Chooser", autoChooser);
   }
+
+
 
   private void configureBindings() {}
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
