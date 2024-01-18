95d0f61a2cd2d08395c71a3584da5389e423936c
MacPGamesAndApps <pcypc@hotmail.com>
2024-01-17 23:55:31 -0500

    More code cleanup.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,30 +4,15 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.ShootCommand;
-import frc.robot.subsystems.ShooterSubsystem;
 
 public class RobotContainer {
-  private final Joystick driver = new Joystick(0);
-  private final Trigger driverLeftTrigger = new Trigger (() -> driver.getRawAxis(XboxController.Axis.kLeftTrigger.value) >0.1);
-  private final ShooterSubsystem shooter = new ShooterSubsystem();
-  private final JoystickButton driverYButton = new JoystickButton(driver, XboxController.Button.kY.value);
-  private final JoystickButton driverAButton = new JoystickButton(driver, XboxController.Button.kA.value);
-  
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {
-    driverLeftTrigger.whileTrue(new ShootCommand(() -> driver.getRawAxis (XboxController.Axis.kLeftTrigger.value), shooter));
-    //driverYButton.onTrue()
-  }
+  private void configureBindings() {}
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
