8aefadbfafdbea5b42aeb65d14ef25c363545709
makeshift4039student <makeshiftrobotics@gmail.com>
2024-01-19 16:13:42 -0500

    Add shooter PID Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -159,5 +159,10 @@ public final class Constants {
   public static final class ShooterConstants {
     public static final int leftShooterCANId = 30;
     public static final int rightShooterCANId = 31;
+
+    public static final double kShooterP = 0.0;
+    public static final double kShooterI = 0.0;
+    public static final double kShooterD = 0.0;
+    public static final double kShooterFF = 0.0;
   }
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import frc.robot.Constants.ShooterConstants;
 
@@ -14,12 +15,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterSubsystem extends SubsystemBase {
   private final CANSparkFlex m_leftShooterVortex;
   private final CANSparkFlex m_rightShooterVortex;
+  private final SparkPIDController m_leftShooterController;
+  private final SparkPIDController m_rightShooterController;
 
  
  /**Create motor elements.*/
   public ShooterSubsystem() {
     m_leftShooterVortex = new CANSparkFlex(ShooterConstants.leftShooterCANId, MotorType.kBrushless);
     m_rightShooterVortex = new CANSparkFlex(ShooterConstants.rightShooterCANId, MotorType.kBrushless);
+
+    m_leftShooterController = m_leftShooterVortex.getPIDController();
+    m_rightShooterController = m_rightShooterVortex.getPIDController();
+
+    m_leftShooterController.setP(ShooterConstants.kShooterP);
+    m_leftShooterController.setI(ShooterConstants.kShooterI);
+    m_leftShooterController.setD(ShooterConstants.kShooterD);
+    m_leftShooterController.setFF(ShooterConstants.kShooterFF);
+
+    m_rightShooterController.setP(ShooterConstants.kShooterP);
+    m_rightShooterController.setI(ShooterConstants.kShooterI);
+    m_rightShooterController.setD(ShooterConstants.kShooterD);
+    m_rightShooterController.setFF(ShooterConstants.kShooterFF);
   }
 
   @Override
@@ -31,6 +47,6 @@ public class ShooterSubsystem extends SubsystemBase {
   public void shooterSpeedControl(double leftSpeed, double rightSpeed, double speedLimit) {
     m_leftShooterVortex.set(leftSpeed * speedLimit);
     m_rightShooterVortex.set(rightSpeed * speedLimit);
-  } 
+  }
 
 }
