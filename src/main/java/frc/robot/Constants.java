a6c4c4c60398d85015f3d44184c5bcacdefdd20e
Ben Alderson <ben.c.alderson@proton.me>
2024-01-29 17:50:38 -0500

    Tweak constants for PID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -184,12 +184,12 @@ public final class Constants {
         public static final double kShooterSpeedLimit = 0.7;
         public static final double kShooterAmpSpeedLimit = 0.7;
 
-        public static final double kShooterP = 0.0;
+        public static final double kShooterP = 0.0005;
         public static final double kShooterI = 0.0;
-        public static final double kShooterD = 0.0;
-        public static final double kShooterFF = 0.1;
+        public static final double kShooterD = 0.001;
+        public static final double kShooterFF = 0.000145;
 
-        public static final double kShooterRPM = 1000;
+        public static final double kShooterRPM = 2000;
     }
 
     public static final class FeederConstants {
