88893c207acb65d59983d300e64c3a0cddf6fcce
Ben Alderson <ben.c.alderson@proton.me>
2024-01-31 18:00:23 -0500

    Add git info to ShuffleBoard

--- src/main/java/frc/robot/utils/Helpers.java
+++ src/main/java/frc/robot/utils/Helpers.java
@@ -1,6 +1,7 @@
 package frc.robot.utils;
 
 import edu.wpi.first.wpilibj.RobotController;
+import frc.robot.BuildConstants;
 
 public class Helpers {
     public static boolean isBlackout() {
@@ -9,5 +10,15 @@ public class Helpers {
 
     public static String getRobotName() {
         return RobotController.getComments();
+    }
+
+    public static String getGitBranch() {
+        String dirtyString = " (dirty)";
+        switch (BuildConstants.DIRTY) {
+            case 0:
+                dirtyString = "";
+        }
+
+        return BuildConstants.GIT_BRANCH + dirtyString;
     }
 }
