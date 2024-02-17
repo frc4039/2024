7157bccd5c7d1a0614bb2319a86a2ae68aa2a95a
marco <marco.sienna@gmail.com>
2024-02-16 19:13:46 -0500

    New beam braker rumble

--- src/main/java/frc/robot/commands/RumbleCommand.java
+++ src/main/java/frc/robot/commands/RumbleCommand.java
@@ -7,30 +7,41 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.IndexerSubsystem;
 
 public class RumbleCommand extends Command {
     private Joystick driverControler;
     private Joystick operatorControler;
 
+    IndexerSubsystem m_Indexer;
+    double StartTime;
+    boolean TimerStarted = false;
+
     /** Creates a new rumble. */
-    public RumbleCommand(Joystick driverControler, Joystick operatorControler) {
+    public RumbleCommand(Joystick driverControler, Joystick operatorControler, IndexerSubsystem Indexer) {
         // Use addRequirements() here to declare subsystem dependencies.
         this.driverControler = driverControler;
         this.operatorControler = operatorControler;
+        m_Indexer = Indexer;
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
         // controler ruble intialize
-        driverControler.setRumble(RumbleType.kBothRumble, 1.0);
-        operatorControler.setRumble(RumbleType.kBothRumble, 1.0);
 
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        if (m_Indexer.hasNote() && !TimerStarted) {
+            driverControler.setRumble(RumbleType.kBothRumble, 1.0);
+            operatorControler.setRumble(RumbleType.kBothRumble, 1.0);
+            TimerStarted = true;
+            StartTime = Timer.getFPGATimestamp();
+        }
     }
 
     // Called once the command ends or is interrupted.
@@ -44,6 +55,6 @@ public class RumbleCommand extends Command {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return (TimerStarted && (Timer.getFPGATimestamp() - StartTime > 0.5)) ? true : false;
     }
 }
