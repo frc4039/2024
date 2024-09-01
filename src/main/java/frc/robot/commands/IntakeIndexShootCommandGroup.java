8cfed7fa389e447595ace9a104de3ae4c7d68238
MakeshiftProgrammingOne <makeshiftprogramming.one@hotmail.com>
2024-09-01 10:17:09 -0400

    auto time out to 2.5 sec, intake limit to 50

--- src/main/java/frc/robot/commands/IntakeIndexShootCommandGroup.java
+++ src/main/java/frc/robot/commands/IntakeIndexShootCommandGroup.java
@@ -20,7 +20,7 @@ public class IntakeIndexShootCommandGroup extends ParallelDeadlineGroup {
             Joystick operatorController) {
         // Add the deadline command in the super() call. Add other commands using
         // addCommands().
-        super(new AutoIndexerShootCommand(shooterSubsystem, indexerSubsystem).withTimeout(3));
+        super(new AutoIndexerShootCommand(shooterSubsystem, indexerSubsystem).withTimeout(2.5));
 
         addCommands(
                 new IntakeCommand(intakeSubsystem));
